cf244625a9b7d4952c8eb7657c41545100674911
viniciusagvaz <agvazvinicius@gmail.com>
2023-08-23 13:37:50 -0300

    projeto 01

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post.jsx";
+
+export function App() {
+	return (
+		<div>
+			<h1>Olá, mundo!</h1>
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+		</div>
+	);
+}
+
