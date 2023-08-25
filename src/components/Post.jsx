f2d4d73754276bd17a3de9a5e37b2787a3e47c79
viniciusagvaz <agvazvinicius@gmail.com>
2023-08-25 11:29:10 -0300

    Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,17 @@
 import { format, formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { useState } from "react";
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
+
 import styles from "./Post.module.css";
 
-// author: {avatar_url: "", name: "", role: ""}
-// publishedAt: Date
-// content: String
+// estado = variáveis que eu quero que o componente monitore
 
 export function Post({ author, publishedAt, content }) {
+	const [comments, setComments] = useState([1, 2]);
+
 	const publishedDateFormatted = format(
 		publishedAt,
 		"d 'de' LLLL 'às' HH:mm'h'",
@@ -20,6 +22,13 @@ export function Post({ author, publishedAt, content }) {
 		locale: ptBR,
 		addSuffix: true,
 	});
+
+	function handleCreateNewComment() {
+		event.preventDefault();
+      
+		setComments([... comments, comments.length + 1]);
+      console.log(comments)
+   }
 
 	return (
 		<article className={styles.post}>
@@ -54,8 +63,9 @@ export function Post({ author, publishedAt, content }) {
 				})}
 			</div>
 
-			<form className={styles.commentForm}>
+			<form onSubmit={handleCreateNewComment} className={styles.commentForm}>
 				<strong>Deixe seu feedback</strong>
+
 				<textarea placeholder="Deixe um comentário"></textarea>
 
 				<footer>
@@ -64,9 +74,9 @@ export function Post({ author, publishedAt, content }) {
 			</form>
 
 			<div className={styles.commentList}>
-				<Comment />
-				<Comment />
-				<Comment />
+				{comments.map(comment => {
+					return <Comment />;
+				})}
 			</div>
 		</article>
 	);
